25a0bf87c74b72e7c59fad17a3cfed68d91ff168
TheMythNick <75760047+TheMythNick@users.noreply.github.com>
2020-12-15 19:27:28 +1300

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>SOLUTIONS THAT WORK THE FIRST TIME!</h1>
+      
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          SIGN UP TO SEE A LIST OF PROJECTS THAT MIGHT INTEREST YOU
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          REQUEST A FREE CONSULTATION <i className='fas fa-wrench' />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
